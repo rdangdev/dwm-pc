af75b6578d0bbf48d93ac208e75ac622c861e139
Ray Dang <ray@rdang.dev>
2022-07-25 20:32:46 +1000

    personal appearance + colours

--- config.def.h
+++ config.def.h
@@ -1,26 +1,26 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
 static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
-static const char *alttrayname      = "tray";    /* Polybar tray instance name */
-static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *alttrayname      = "main";    /* Polybar tray instance name */
+static const char *altbarcmd        = "XDG_CONFIG_HOME/polybar/blocks/launch.sh"; /* Alternate bar launch command */
+static const char *fonts[]          = { "FiraCode Nerd Font Mono:pixelsize=20" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#420709";
+static const char col_gray4[]       = "#190707";
+static const char col_gray3[]       = "#a51201";
+static const char col_gray2[]       = "#caaea2";
+static const char col_cyan[]        = "#800000";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+  /*               fg         bg         border   */
+  [SchemeNorm] = { col_gray2, col_cyan,  col_gray4 },
+  [SchemeSel]  = { col_gray2, col_gray1, col_cyan  },
 };
 
 typedef struct {
